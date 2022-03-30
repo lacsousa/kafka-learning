a601cbbd2e20d854ce5d8cb2d1db0b1977fe0869
Luciano Cordeiro <lacsousa@gmail.com>
2022-03-29 22:52:11 -0300

    Primeiro commit

---
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,40 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+  
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    
+      var producer = new KafkaProducer<String, String>(properties());
+      var value = "111111,22222, 1234567";
+      var producerRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+
+      producer.send(producerRecord, (data, ex) -> {
+                  if (ex != null) {
+                     ex.printStackTrace();
+                    return;
+                  }
+          System.out.println("Sucesso enviando " + data.topic() +
+                  " ::partition " + data.partition() +
+                  "/ offset " + data.offset() +
+                  " /timestamp " + data.timestamp());
+        }).get(); // Precisamos de um callback pra retornar o sucesso ou falha
+  }
+
+  private static Properties properties(){
+    var properties = new Properties();
+
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    
+    return properties;
+  }
+}
